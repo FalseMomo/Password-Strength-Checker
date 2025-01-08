192b3ef68231f00080d653c145ee460e0edd0acf
Mohamed Amechtak <dragonickhalil95@gmail.com>
2025-01-08 16:43:36 -0500

    added set access, added more special characters, implemented error handling

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ def strength_checker(password, filename="Password-Strength-Checker/10-million-pa
     """
 
     # parses a common passwords text file to check if the given password is a common password
-    with open(filename) as file:
+    try:
+        with open(filename) as file:
+            word_set = set() # uses a set for faster access to words
+            for line in file:
+                word = line.strip()
+                word_set.add(word)
+                if password in word_set:
+                    return 'This password has been identified as a common password. Please use a different password!'
 
-        for line in file:
-            word = line.strip()
-            if word == password:
-                return 'This password has been identified as a common password. Please use a different password!'
+    # handles FileNotFound error                
+    except FileNotFoundError:
+        return "Cannot find the common passwords text file!"
             
     # checks if the length of the password is at least 16 characters long (according to cisa.gov)
     if len(password) < 16:
@@ -27,7 +33,7 @@ def strength_checker(password, filename="Password-Strength-Checker/10-million-pa
         return 'This password must contain at least one digit. Please use a different password!'
     
     # checks if there is at least 1 special character in the password (according to cisa.gov)
-    if not re.findall('[!@$%^&*+#;:<>?,.~`]+', password):
+    if not re.findall('[!@$%^&*+#;:<>?,.~`()-_={/}[]|\\]+', password):
         return 'This password must contain at least one special character. Please use a different password!'
     
     return 'This password is strong.'
